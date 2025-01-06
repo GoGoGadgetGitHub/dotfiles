a6a408a52599d8176bda007049ff11a7ec7169d3
GoGoGadgetGitHub <christobothma258@gmail.com>
2025-01-06 05:53:19 +0200

    rofi and some keybinds i think

--- .config/hypr/scripts/change-wallpaper.sh
+++ .config/hypr/scripts/change-wallpaper.sh
@@ -4,16 +4,19 @@
 waypaper --random 
 
 #wait a bit
-sleep 0.5
+sleep 0.3
+
+#restart waybar (cuase idk the style reload broke somehow
+killall waybar && hyprctl dispatch exec waybar
 
 #update swaync style
-swaync-client --reload-css 
+swaync-client --reload-css &&
 
 #update pywal fox
-pywalfox update 
+pywalfox update &&
 
 #run script to put current wallpaper in a folder to host a local server from
-~/.config/waypaper/wallpaper_server.sh
+~/.config/waypaper/wallpaper_server.sh &&
 
 #refresh discord
-hyprctl dispatch sendshortcut CONTROL, r, vesktop
+hyprctl dispatch sendshortcut CONTROL, r, vesktop &&
